9e36c460afd10c477da92af11b5bd90d6f67049d
MadStoddard <madelinejdierking@gmail.com>
2022-03-31 21:28:29 -0400

    added cat api

--- Develop/assets/JS/script.js
+++ Develop/assets/JS/script.js
@@ -1,1 +1,10 @@
 
+var catApi = "https://api.thecatapi.com/v1/images/search"
+
+fetch(catApi)
+.then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+
+    });
+});
