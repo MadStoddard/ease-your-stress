731f50643dd5802bf3e87a8f305501e743430b92
MadStoddard <madelinejdierking@gmail.com>
2022-04-02 11:01:50 -0400

    added in image button & pushed cat API

--- Develop/assets/JS/script.js
+++ Develop/assets/JS/script.js
@@ -10,12 +10,16 @@ console.log(Greatful);
 localStorage.setItem('greatful', JSON.stringify(Greatful));
 }
 
-var catApi = "https://api.thecatapi.com/v1/images/search"
-
+var catApi = "https://api.thecatapi.com/v1/images/search?api_key=64449b51-6a0d-4e9c-be78-0517d1e9f6a7"
 fetch(catApi)
 .then(function(response) {
     response.json().then(function(data) {
         console.log(data);
+        var catImgUrl = data[0].url;
+        console.log(catImgUrl)
+        var catImage = document.querySelector(".cat-image");
+        catImage.textContent = catImgUrl;
+
 
     });
 });
