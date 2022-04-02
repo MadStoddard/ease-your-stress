9f0d5a2245922d421ec105ceb859503b7b644e95
MadStoddard <madelinejdierking@gmail.com>
2022-04-02 11:25:08 -0400

    cat pictures appear & circulate

--- Develop/assets/JS/script.js
+++ Develop/assets/JS/script.js
@@ -10,16 +10,24 @@ console.log(Greatful);
 localStorage.setItem('greatful', JSON.stringify(Greatful));
 }
 
-var catApi = "https://api.thecatapi.com/v1/images/search?api_key=64449b51-6a0d-4e9c-be78-0517d1e9f6a7"
-fetch(catApi)
-.then(function(response) {
-    response.json().then(function(data) {
-        console.log(data);
-        var catImgUrl = data[0].url;
-        console.log(catImgUrl)
-        var catImage = document.querySelector(".cat-image");
-        catImage.textContent = catImgUrl;
 
-
+function loadImg() {
+    var catApi = "https://api.thecatapi.com/v1/images/search?api_key=64449b51-6a0d-4e9c-be78-0517d1e9f6a7";
+    var imageDiv = document.querySelector(".cat-image");
+    fetch(catApi)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        for (var i=0; i <data.length; i++) {
+            var imageEl = document.createElement('img');
+            imageEl.src = data[i].url;
+            imageDiv.append(imageEl);
+        }
     });
-});
+    
+
+}
+
+loadImg();
+
