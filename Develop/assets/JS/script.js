50026fd1a16bc38ecc36f684ff3c8cfc813bbfce
MadStoddard <madelinejdierking@gmail.com>
2022-04-02 22:33:29 -0400

    added css styling

--- Develop/assets/JS/script.js
+++ Develop/assets/JS/script.js
@@ -1,16 +1,16 @@
-const Greatful = [];
+const Grateful = [];
 var btn = document.getElementById('btn');    
 var text = document.getElementById('Gratitude-text');
-if (localStorage.getItem("greatful")){
-    Greatful.push(localStorage.getItem('greatful'));
+if (localStorage.getItem("grateful")){
+    Grateful.push(localStorage.getItem('grateful'));
 }
-function greatfulSave () {
-Greatful.push(text.value);
-console.log(Greatful);
-localStorage.setItem('greatful', JSON.stringify(Greatful));
+function gratefulSave () {
+Grateful.push(text.value);
+console.log(Grateful);
+localStorage.setItem('grateful', JSON.stringify(Grateful));
 }
 
-
+// random cat image
 function loadImg() {
     var catApi = "https://api.thecatapi.com/v1/images/search?api_key=64449b51-6a0d-4e9c-be78-0517d1e9f6a7";
     var imageDiv = document.querySelector(".cat-image");
@@ -25,9 +25,10 @@ function loadImg() {
             imageDiv.append(imageEl);
         }
     });
-    
+};
 
-}
+var catBtn = document.querySelector("#image-btn");
+catBtn.addEventListener('click', loadImg);
 
-loadImg();
+
 
