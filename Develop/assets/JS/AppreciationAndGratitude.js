2148dbf9a96feac7d8f16360488dcc9eb0e0c80d
Beepo <micahstew@gmail.com>
2022-04-05 20:03:02 -0400

    adding more function

--- Develop/assets/JS/AppreciationAndGratitude.js
+++ Develop/assets/JS/AppreciationAndGratitude.js
@@ -4,7 +4,7 @@ var AppBtn = document.getElementById('AppreciationSubmit');
 var AppText = document.getElementById('Appreciation-prompt');
 var AppreciationSection = document.getElementById('Appreciation');
 var GratitudeSection = document.getElementById('Gratitude');
-
+var AppTextArea = document.getElementById('AppTextArea');
 function ImGratefulForSubmit (){
 
     AppText.innerHTML = `I Appreciate ${GratText.value} Because `;    
@@ -12,4 +12,20 @@ function ImGratefulForSubmit (){
     GratitudeSection.style.display = 'none';
 }
 
+function AppreciationSubmit() {
+    console.log(GratText.value)
+    localStorage.setItem(GratText.value, AppTextArea.value);
+
+    for (var x = 29; x > 0; x--){
+        if(localStorage.getItem(`I'm Greatful For${x}`)){
+
+        } else if (localStorage.getItem(`I'm Greatful For0`)){
+
+        } else {
+            localStorage.setItem(`I'm Greatful For0`, AppTextArea.value)
+        }
+    }
+}
+
 GratBtn.addEventListener('click', ImGratefulForSubmit);
+AppBtn.addEventListener('click', AppreciationSubmit);
